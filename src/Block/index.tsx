a641b6db2c8978e1ac04b3087ca67711f6574727
nsne <15527632169@163.com>
2024-07-07 14:46:43 +0800

    chore: delete uppercase file

--- src/Block/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createVirtualBox } from '@formily/react-schema-renderer';
-
-export const FormBlock = createVirtualBox('block', ({ children, ...props }) => {
-  return <div {...props} className="formily-block">{children}</div>
-});
-
-export default FormBlock;
--- src/Card/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createVirtualBox } from '@formily/react-schema-renderer';
-
-export const FormCard = createVirtualBox('card', ({ children, ...props }) => {
-  return <div {...props}>{children}</div>
-});
-
-export default FormCard;
